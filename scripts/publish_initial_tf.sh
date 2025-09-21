7c024ef9071fc74c9705660fa748ecea2e4fac2e
Carlos Tojal <carlos.c.tojal@gmail.com>
2025-09-21 02:20:32 +0100

    allow additional script arguments

--- scripts/publish_initial_tf.sh
+++ scripts/publish_initial_tf.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 FIXED_FRAME(odom/map/...)"
+if [ "$#" -lt 1 ]; then
+    echo "Usage: $0 FIXED_FRAME(odom/map/...). Got $# arguments"
     exit
 fi
 rosrun tf2_ros static_transform_publisher 0.935 1.34 -0.023 0.001 -0.003 0.737 0.676 mocap $1
